3ea6399cf39df7e6e031ec1c4285755710a5bc12
Lord Ezen <mo23274@bristol.ac.uk>
2025-07-28 14:04:25 +0100

    File cleanup

---
+++ orderbooksimulator/main.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <chrono>
+#include <thread>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "order.hpp"
+#include "orderBook.hpp"
+#include "orderGenerator.hpp"
+#include "threadManager.hpp"
+#include "utils.hpp"
+
+//Template definition with Order as the typename
+ThreadSafe<Order> orderQueue;
+
+//producer : produce a random order and push it into orderqueue
+void produceOrder(int count) {
+    OrderGenerator generator;
+    for (int i=0; i < count; i++){
+        Order order = generator.generateOrder();
+        orderQueue.push(order);
+    }
+
+}
+
+
+
+//consumer : consumes (pop) order from queue and and process said order 
+void consumeOrder(OrderBook& book, int totalOrders, size_t& tradecount, std::ofstream& filename, std::mutex& logMutex, bool logEnabled){
+    std::stringstream localBuffer;
+    for (int i=0; i < totalOrders; i++){
+        Order order;
+        orderQueue.pop(order);
+        book.addOrder(order);
+        book.matchOrder(tradecount, localBuffer, logEnabled);
+
+    }
+
+    std::lock_guard<std::mutex> lock(logMutex);
+    filename << localBuffer.str();
+
+}
+
+int main(int argc, char* argv[]) {
+    OrderBook book;
+
+    const int TOTAL_ORDERS = 1000000;
+    bool logEnabled = false;
+
+    for (int i=1; i<__argc;i++){
+        std::string arg = argv[i];
+        if (arg == "--log"){
+            logEnabled = true;
+        }
+    }
+
+    //static trade statistics
+    size_t tradecount = 0;
+
+    //logging trades
+    std::ofstream tradeLog("trades.csv");
+    tradeLog << "quantity, price, buyer_ID, seller_ID\n";
+
+    //Define timer to benchmark
+    Timer timer;
+    timer.begin();
+
+    //number of consumer thread (max hardware threads - no.of producer threads)
+    int num_consumers = std::thread::hardware_concurrency() - 1;
+    std::mutex logMutex;
+
+    //consumer runs (process orders with orderbook) with equal work on each cores
+    //vector used to join threads for speed, list is too slow
+    std::vector<std::thread> consumers;
+    for (int i =0; i < num_consumers;i++){
+        consumers.emplace_back(consumeOrder, std::ref(book), TOTAL_ORDERS/num_consumers, std::ref(tradecount), std::ref(tradeLog), std::ref(logMutex), logEnabled);
+    }
+
+
+    //producer runs (generate random orders)
+    std::thread producer(produceOrder, TOTAL_ORDERS);
+    producer.join();
+    
+    for (auto& t : consumers){
+        t.join();
+    }
+
+    double total_time = timer.end();
+    tradeLog.close();
+
+    //Basic benchmarking
+    std::cout << std::endl << "Processed " << TOTAL_ORDERS << " orders in " << total_time << " seconds" << std::endl;
+    std::cout << tradecount << " trades made in total." << std::endl;
+    std::cout<< "Using " << num_consumers << " consumers threads."<< std::endl; 
+    
+    return 0;
+
+}
